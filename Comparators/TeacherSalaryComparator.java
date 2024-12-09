8f85ba8f2ed3fa5089586755f8e736672ccdc6fb
riririririiiiii <145469004+riririririiiiii@users.noreply.github.com>
2024-12-09 21:29:42 +0500

    upd

--- Comparators/TeacherSalaryComparator.java
+++ Comparators/TeacherSalaryComparator.java
@@ -1,5 +1,14 @@
 package Comparators;
 
-public class TeacherSalaryComparator {
+import java.util.Comparator;
+
+import Users.Teacher;
+
+public class TeacherSalaryComparator implements Comparator<Teacher> {
+
+	@Override
+	public int compare(Teacher t1, Teacher t2) {
+		return Double.compare(t1.getSalary(), t2.getSalary());
+	}
 
 }
