9322aed5676329ac31daeca33211ffb6d5af333b
riririririiiiii <145469004+riririririiiiii@users.noreply.github.com>
2024-12-09 21:29:13 +0500

    upd

--- Comparators/TeacherNameComparator.java
+++ Comparators/TeacherNameComparator.java
@@ -1,5 +1,11 @@
 package Comparators;
 
-public class TeacherNameComparator {
+import java.util.Comparator;
 
+import Users.Teacher;
+
+public class TeacherNameComparator implements Comparator<Teacher> {
+    public int compare(Teacher t1, Teacher t2) {
+        return t1.getName().compareToIgnoreCase(t2.getName());
+    }
 }
