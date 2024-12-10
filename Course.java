4b6abeef9d92922cb86256ea11b64ef112d9e9f0
Aidyn <155320055+yessenzhanuly@users.noreply.github.com>
2024-12-10 10:41:13 +0500

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,7 +1,6 @@
-package SystemParts;
+package Systemparts;
 
 import Users.Student;
-import Users.Teacher;
 
 import java.util.*;
 
@@ -13,37 +12,106 @@ public class Course {
     private List<Teacher> instructors;
     private List<Student> students;
     private List<Lesson> lessons;
+    private Map<Student, Mark> studentMarks;
 
-    // Конструктор
     public Course(String id, String name, Integer credits, String major) {
         this.id = id;
         this.name = name;
         this.credits = credits;
         this.major = major;
-        this.teachers = new ArrayList<>();
+        this.instructors = new ArrayList<>();
         this.students = new ArrayList<>();
         this.lessons = new ArrayList<>();
+        this.studentMarks = new HashMap<>();
     }
 
-    // Геттеры
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
-    // Методы
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
     public void addStudent(Student student) {
         students.add(student);
+        studentMarks.put(student, new Mark(student, this));
     }
 
     public void removeStudent(Student student) {
         students.remove(student);
+        studentMarks.remove(student);
     }
 
-    public void assignTeacher(Teacher teacher) {
-        teachers.add(teacher);
+    public void assignInstructor(Teacher teacher) {
+        instructors.add(teacher);
+    }
+
+    public void removeTeacher(Teacher teacher) {
+        instructors.remove(teacher);
     }
 
     public void addLesson(Lesson lesson) {
         lessons.add(lesson);
+    }
+
+    public void removeLesson(Lesson lesson) {
+        lessons.remove(lesson);
+    }
+
+    public Set<Teacher> getInstructors() {
+        return new HashSet<>(instructors);
+    }
+
+    public Set<Lesson> getLessons() {
+        return new HashSet<>(lessons);
+    }
+
+    public void putAttestationMarks(Student student, double firstAttestation, double secondAttestation) {
+        Mark mark = studentMarks.get(student);
+        if (mark != null) {
+            mark.setAttestationMarks(firstAttestation, secondAttestation);
+            //System.out.println("Attestation marks set for " + student.getName() + " in " + name);
+        } else {
+            System.out.println("Student not enrolled in this course.");
+        }
+    }
+
+    public void putFinalExamMark(Student student, double finalExam) {
+        Mark mark = studentMarks.get(student);
+        if (mark != null) {
+            mark.setFinalExamMark(finalExam);
+            //System.out.println("Final exam mark set for " + student.getName() + " in " + name);
+        } else {
+            System.out.println("Student not enrolled in this course.");
+        }
+    }
+
+    public Mark getStudentMark(Student student) {
+        return studentMarks.get(student);
     }
 }
