771ac873bb2cc3c53065994f2aa21c99defe9c0e
Aidyn <155320055+yessenzhanuly@users.noreply.github.com>
2024-12-21 23:03:59 +0500

    Update Course.java

--- SystemParts/Course.java
+++ SystemParts/Course.java
@@ -2,31 +2,44 @@ package SystemParts;
 
 import Users.Student;
 import Users.Teacher;
-
 import java.util.*;
 
 public class Course {
+
+    public enum CourseType {
+        MAJOR,
+        MINOR,
+        ELECTIVE
+    }
+
     private String id;
     private String name;
     private Integer credits;
-    private String major;
+    private String school;
+    private CourseType type;
     private List<Teacher> instructors;
-    private List<Student> students;
     private List<Lesson> lessons;
     private Map<Student, Mark> studentMarks;
 
-    public Course(String id, String name, Integer credits, String major) {
+    public Course(String id, String name, Integer credits, String school, CourseType type) {
         this.id = id;
         this.name = name;
         this.credits = credits;
-        this.major = major;
+        this.school = school;
+        this.type = type;
         this.instructors = new ArrayList<>();
-        this.students = new ArrayList<>();
         this.lessons = new ArrayList<>();
         this.studentMarks = new HashMap<>();
     }
 
-    // Getters and Setters
+    public CourseType getType() {
+        return type;
+    }
+
+    public void setType(CourseType type) {
+        this.type = type;
+    }
+
     public String getId() {
         return id;
     }
@@ -51,33 +64,22 @@ public class Course {
         this.credits = credits;
     }
 
-    public String getMajor() {
-        return major;
+    public String getSchool() {
+        return school;
     }
 
-    public void setMajor(String major) {
-        this.major = major;
-    }
-
-    public void addStudent(Student student) {
-        students.add(student);
-        studentMarks.put(student, new Mark(student, this));
-    }
-
-    public void removeStudent(Student student) {
-        students.remove(student);
-        studentMarks.remove(student);
+    public void setSchool(String school) {
+        this.school = school;
     }
 
     public void assignInstructor(Teacher teacher) {
         instructors.add(teacher);
     }
 
-    public void removeTeacher(Teacher teacher) {
+    public void removeInstructor(Teacher teacher) {
         instructors.remove(teacher);
     }
 
-    // Methods for lessons
     public void addLesson(Lesson lesson) {
         lessons.add(lesson);
     }
@@ -94,13 +96,20 @@ public class Course {
         return new HashSet<>(lessons);
     }
 
+    public void addStudent(Student student) {
+        studentMarks.put(student, new Mark(student, this));
+    }
+
+    public void removeStudent(Student student) {
+        studentMarks.remove(student);
+    }
+
     public void putAttestationMarks(Student student, double firstAttestation, double secondAttestation) {
         Mark mark = studentMarks.get(student);
         if (mark != null) {
             mark.setAttestationMarks(firstAttestation, secondAttestation);
-            //System.out.println("Attestation marks set for " + student.getName() + " in " + name);
         } else {
-            System.out.println("Student not enrolled in this course.");
+            System.out.println("Student is not registered for the course");
         }
     }
 
@@ -108,18 +117,12 @@ public class Course {
         Mark mark = studentMarks.get(student);
         if (mark != null) {
             mark.setFinalExamMark(finalExam);
-            //System.out.println("Final exam mark set for " + student.getName() + " in " + name);
         } else {
-            System.out.println("Student not enrolled in this course.");
+            System.out.println("Student is not registered for the course");
         }
     }
 
     public Mark getStudentMark(Student student) {
         return studentMarks.get(student);
     }
-
-	public void viewInstructorsInfo() {
-		// TODO Auto-generated method stub
-		
-	}
 }
