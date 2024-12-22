4750f2336863a070b0d545e2d43c465617dc80c7
Nazerke <157003668+darlxxvii@users.noreply.github.com>
2024-12-22 12:15:39 +0500

    Update Schools.java

--- Enums/Schools.java
+++ Enums/Schools.java
@@ -1,10 +1,15 @@
 package Enums;
 
-public enum Schools {
+import java.util.List;
+
+public enum Schools {	
     SITE("School of Information Technology and Engineering"),
     KMA("Kazakhstan Maritime Academy"),
     SG("School of Geology"),    
     SAM("School of Chemical Engineering");
+	private Schools schoolName;
+    private List<EducationalProgram> availablePrograms;
+
     
 
     private final String fullName;
@@ -15,5 +20,31 @@ public enum Schools {
 
     public String getFullName() {
         return fullName;
+    }
+
+    public boolean isProgramAvailable(EducationalProgram program) {
+        boolean isAvailable = availablePrograms.contains(program);
+
+        if (isAvailable) {
+            switch (schoolName) {
+                case SITE:
+                    return program == EducationalProgram.IS ||
+                           program == EducationalProgram.AC ||
+                           program == EducationalProgram.CSS ||
+                           program == EducationalProgram.ITM ||
+                           program == EducationalProgram.RM;
+                case SG:
+                    return program == EducationalProgram.GESMD ||
+                           program == EducationalProgram.GOG;
+                case KMA:
+                    return program == EducationalProgram.NS ||
+                           program == EducationalProgram.ME;
+                case SAM:
+                    return program == EducationalProgram.CTOS;
+                default:
+                    return false;
+            }
+        }
+        return false;
     }
 }
