577cc4b27865236539551242be8a08e1c5e8fa12
Nazerke <157003668+darlxxvii@users.noreply.github.com>
2024-12-22 20:06:17 +0500

    Update PhDStudent.java

--- Users/PhDStudent.java
+++ Users/PhDStudent.java
@@ -4,16 +4,18 @@ import java.util.List;
 
 import Enums.DegreeLevel;
 import Enums.EducationalProgram;
+import Enums.Schools;
 import Research.ResearchPaper;
 import Research.Researcher;
+import Research.ResearcherHelper;
 import SystemParts.School;
 
 public class PhDStudent extends Student implements Researcher{
 
     private int credits;
     private int yearOfStudy;
-
-    public PhDStudent(String name, String surname, String email, int enrollmentYear,School school, EducationalProgram educationalProgram) {
+    private ResearcherHelper researcherHelper = new ResearcherHelper();
+    public PhDStudent(String name, String surname, String email, int enrollmentYear,Schools school, EducationalProgram educationalProgram) {
         super(name, surname, email, enrollmentYear, DegreeLevel.PHD, school, educationalProgram);  
     }
 
@@ -29,33 +31,33 @@ public class PhDStudent extends Student implements Researcher{
         return credits >= 19 && yearOfStudy == 3;
     }
 
-	@Override
-	public void conductResearch(String topic) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void conductResearch(String topic) {
+        researcherHelper.conductResearch(topic);
+    }
 
-	@Override
-	public void publishPaper(ResearchPaper paper) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void publishPaper(ResearchPaper paper) {
+        researcherHelper.publishPaper(paper);
+    }
 
-	@Override
-	public List<ResearchPaper> getPublishedPapers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<ResearchPaper> getPublishedPapers() {
+        return researcherHelper.getPublishedPapers();
+    }
 
-	@Override
-	public int calculateHIndex() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int calculateHIndex() {
+        return researcherHelper.calculateHIndex();
+    }
 
-	@Override
-	public int getTotalCitations() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getTotalCitations() {
+        return researcherHelper.getTotalCitations();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
 }
